ac55b508e49f838ee398b58b35e597e704f989ab
Edidiong-Edet <98217550+Edidiong-Edet@users.noreply.github.com>
2022-01-22 13:36:22 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const btn=document.getElementById('Btn')
+const side=document.getElementById('Sidebar')
+
+
+function show(){
+    side.classList.toggle('motion')
+    side.style.backgroundColor='red'
+
+}
+
+
+
